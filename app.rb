d4b58ab2b9e041c3a7e9b09f1405d03c39a1cef6
Abdul Munim kazia <munim@browserstack.com>
2016-09-24 01:10:42 +0530

    messaging

--- app.rb
+++ app.rb
@@ -1,6 +1,32 @@
 require 'sinatra'
+require 'redis'
+require 'sinatra/json'
+require 'geo-distance'
+
+
+users = []
+messages = []
 
 get '/' do
-          'Hello world!'
+	'Hello world!'
 end
 
+get '/users' do
+	json users
+end
+
+get '/messages' do
+	json messages
+end
+
+post '/message' do
+	id = params[:id]
+	message = params[:message]
+	messages << {:id => id, :message => message }
+	json true
+end
+
+get '/distance' do
+	distances = []
+	json users.combination(2).to_a
+end
